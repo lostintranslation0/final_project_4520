c0a165d8e03df3c1dab230014c2905c30c55d7f1
Dylan McNulty <mcnulty.d@northeastern.edu>
2023-11-21 23:04:02 -0500

    did add blog button and fragment, and user public profile

---
+++ app/src/main/java/com/example/finalproject/ui/blogs/CreateBlogFragment.java
@@ -0,0 +1,21 @@
+package com.example.finalproject.ui.blogs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.finalproject.R;
+
+public class CreateBlogFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_blog, container, false);
+    }
+}
